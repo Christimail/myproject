f7d380566add9b5071a6ef13010ba941d7170f38
Christina <example@gmail.com>
2021-09-02 02:34:48 +0300

    first commit

---
+++ js/js/script.js
@@ -0,0 +1,101 @@
+ /*
+ " use strict";
+
+a =15;
+console.log(a);
+
+let number = 5;
+const leftlBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj );
+
+console.log(name);
+var name = 'Christina';
+
+{
+      var result = 50;
+}
+
+console.log(result); 
+
+
+
+
+"use strict";
+
+number = 4.6;
+
+console.log(-4/0);
+console.log('string' * 9);
+
+const persone = "Christina";
+
+const bool = true;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+let obj = {
+    name: `John`,
+    age: 25,
+    isMarried: false
+};
+console.log(obj [`name`]);
+
+let arr = [`plum.png`, `orange.jpg`, ` apple.bmp`, {}, []];
+ console.log(arr[1]);
+
+ 
+*/
+
+
+//const answers = [];
+
+//answers[0] = prompt("Как ваше имя?", "");
+//answers[1] = prompt("Как ваша фамилия?","");
+//answers[2] = prompt("Сколько вам лет?", "");
+
+//console.log(typeof(answers));
+
+//"use sctrict";
+
+//const category = "toys";
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Christina";
+
+//alert(`Привет, ${user}`);
+
+"use strict";
+
+//console.log('arr' + " - object");
+//console.log(4 + + "5");
+
+//let incr = 10,
+  //  decr = 10;
+
+//    incr++;
+//    decr--;
+
+//console.log(++incr);
+//console.log(--decr);
+
+//console.log(5%2);
+
+//console.log(2 + 2 * 2 !=);
+
+
+//const isChecked = false,
+   // isClose = false;
+
+//console.log(isChecked || !isClose);
